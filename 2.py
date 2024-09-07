dc26acd532ffdbe53f74d1690c17f6b3807f2ae8
Roman <rudin.roman@bk.ru>
2024-09-07 02:36:58 +0200

    .gitignore changed, first lab finished and polished (Python), second lab started (Python)

--- 2.py
+++ 2.py
@@ -1,4 +1,6 @@
 #Using cide from previous lab we can check if user input is correct in sense of correct brackets:
-from .lab_1 import *
+from lab_1 import check_brackets
 
 expression = input("Please, enter the expression: ").replace(" ", "")
+if check_brackets(expression, {")":"("}, print_correctness=False, accept_num=True):
+    pass
--- lab_1.py
+++ lab_1.py
@@ -6,8 +6,9 @@ brackets_info = {
 }
 
 # Printing error messages because it looks great
-def printing_errors(string: str, position: int, text="Некорректная скобка!") -> None:
-            print("Строка не существует!")
+def printing_errors(string: str, position: int, print_correctness: bool, text="Incorrect bracket!") -> None:
+            if print_correctness: print("Строка не существует!")
+            print("An error occured While checking brackets")
             print(string)
             print("^".rjust(position + 1))
             print(text)
@@ -21,24 +22,24 @@ def check_brackets(brackets: str, brackets_info: dict, print_correctness=True, p
             lifo.put(bracket)
         elif bracket in brackets_info.keys():
             if (lifo.empty()) or (brackets_info[bracket] != lifo.get()):
-                if print_errors: printing_errors(brackets, position)
+                if print_errors: printing_errors(brackets, position, print_correctness)
                 return False
         else:
-            if (not accept_alpha) and ():
-                if print_errors: printing_errors(brackets, position, text="Некорректный символ!")
+            if (not accept_num) and (bracket in "0123456789+-/*."):
+                if print_errors: printing_errors(brackets, position, print_correctness, text="Incorrect symbol! Expexted bracket, number or arithmerical operation.")
                 return False
-            if (not accept_num) and ():
-                if print_errors: printing_errors(brackets, position, text="Некорректный символ!")
+            if (not accept_alpha) and (bracket.isalpha()):
+                if print_errors: printing_errors(brackets, position, print_correctness, text="Incorrect symbol! Expexted bracket or alpha.")
                 return False
     else:
         if lifo.empty():
             if print_correctness: print("Строка существует")
             return True
         else:
-            if print_errors: printing_errors(brackets, brackets.rfind(lifo.get()), text="Скобка не была закрыта!")
+            if print_errors: printing_errors(brackets, brackets.rfind(lifo.get()), print_correctness, text="The bracket was never closed!")
             return False
             
 
 # Main code
-brackets = input("Пожалуйста, введите строку: ")
-check_brackets(brackets, brackets_info)
+#brackets = input("Пожалуйста, введите строку: ")
+#check_brackets(brackets, brackets_info)
