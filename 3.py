c6e10fb42fba1c346a9b4b22c41bc5a57586ea53
Svetozar <svetozar.kravchuk@gmail.com>
2024-09-12 10:11:57 +0200

    done

--- 3.py
+++ 3.py
@@ -1,23 +1,23 @@
 # Функция поиска вариантов для условия, что i = 3**K * 5**L * 7**M
-def find_variants(i):
+import math
+
+
+def find_variants(x):
     variants = []
-    for K in range(int(i/3)+1):
-        for L in range(int(i/5)+1):
-            for M in range(int(i/7)+1):
-                if 3**K * 5**L * 7**M == i:
-                    variants.append((K, L, M))
+    for K in range(int(math.log(x, 3)) + 1):
+        for L in range(int(math.log(x, 5))+1):
+            for M in range(int(math.log(x, 7))+1):
+                if 3**K*5**L*7**M <= x:
+                    variants.append(3**K*5**L*7**M)
+    variants.sort()
     return variants
 
 
-x = int(input("Введите x, он должен быть натуральным числом!: "))
+x = int(input("Введите x, он должен быть натуральным числом: "))
 if x <= 0:
     print("x должен быть натуральным числом!")
 else:
-    for i in range(1, x + 1):
-        variants = find_variants(i)
-        print(f"Число {i}:")
-        if variants:
-            for K, L, M in variants:
-                print("K:", K, "L:", L, "M:", M, "i:", i)
-        else:
-            print(f"Число {i} не подходит :(")
+    result = []
+    result = find_variants(x)
+
+    print(result)
