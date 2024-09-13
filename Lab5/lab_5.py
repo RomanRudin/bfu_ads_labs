1e11a34e4f85f0773d5294d26e91b7efbca4f968
Roman <rudin.roman@bk.ru>
2024-09-13 09:51:41 +0200

    5th lab done easily

---
+++ Lab5/lab_5.py
@@ -0,0 +1,9 @@
+# Check insertion sort firstly
+
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j - 1]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+            j -= 1
+    return array
--- run_file.py
+++ run_file.py
@@ -1,3 +1,5 @@
 from tests import testing_sorting
 from Lab4.lab_4 import comb_sort
-testing_sorting(comb_sort, max_len=10)
+from Lab5.lab_5 import insertion_sort
+testing_sorting(comb_sort, max_len=100)
+testing_sorting(insertion_sort, max_len=100)
