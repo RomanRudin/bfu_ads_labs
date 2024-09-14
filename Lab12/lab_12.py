0e53e23de8c6b777a1625c388aaa794088bfba3f
Roman <rudin.roman@bk.ru>
2024-09-15 01:49:07 +0200

    9th and 12th labs are done. In need of refactoring on both

--- Lab12/lab_12.py
+++ Lab12/lab_12.py
@@ -1,2 +1,17 @@
-def external_merge_sort(arr:list) -> list:
-    pass
+def external_multiphase_sort(arr: list) -> list:
+    # fibonacci series is used to find the distance of each phase
+    fibonacci_series = [0, 1]  
+    while fibonacci_series[-1] < len(arr):  
+        fibonacci_series.append(fibonacci_series[-1] + fibonacci_series[-2])  
+  
+    def sort_phase(arr, distance):  
+        for i in range(distance, len(arr)):  
+            j = i  
+            while j > 0 and arr[j] < arr[j - distance]:  
+                arr[j], arr[j - distance] = arr[j - distance], arr[j]  
+                j -= distance  
+  
+    # Sorting each phase independently
+    for distance in fibonacci_series[::-1]:  
+        sort_phase(arr, distance)  
+    return arr
--- Lab9/lab_9.py
+++ Lab9/lab_9.py
@@ -1,44 +1,22 @@
-# def heap_sort(arr:list) -> list:
-    # pass
+
+# from heapq import heapify
 # Процедура для преобразования в двоичную кучу поддерева с корневым узлом i, что является индексом в arr[]. n - размер кучи
 def heapify(arr, n, i):
     largest = i # Initialize largest as root
     l = 2 * i + 1   # left = 2*i + 1
     r = 2 * i + 2   # right = 2*i + 2
-
-  # Проверяем существует ли левый дочерний элемент больший, чем корень
-
     if l < n and arr[i] < arr[l]:
         largest = l
-
-    # Проверяем существует ли правый дочерний элемент больший, чем корень
-
     if r < n and arr[largest] < arr[r]:
         largest = r
-
-    # Заменяем корень, если нужно
     if largest != i:
         arr[i],arr[largest] = arr[largest],arr[i] # свап
-
-        # Применяем heapify к корню.
         heapify(arr, n, largest)
 
-# Основная функция для сортировки массива заданного размера
-def heapSort(arr):
-    n = len(arr)
-
-    # Построение max-heap.
-    for i in range(n, -1, -1):
-        heapify(arr, n, i)
-
-    # Один за другим извлекаем элементы
-    for i in range(n-1, 0, -1):
+def heap_sort(arr:list) -> list:
+    for i in range(len(arr), -1, -1):
+        heapify(arr, len(arr), i)
+    for i in range(len(arr)-1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i] # свап 
         heapify(arr, i, 0)
-
-# Управляющий код для тестирования
-arr = [ 12, 11, 13, 5, 6, 7]
-heapSort(arr)
-n = len(arr)
-print ("Sorted array is")
-for i in range(n):
+    return arr
