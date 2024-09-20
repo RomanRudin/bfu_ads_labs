e859fa9d7ed9acde78a5f0d11d17770594d0caf0
Roman <rudin.roman@bk.ru>
2024-09-20 13:58:41 +0200

    Minor changes

--- Lab12/lab_12.py
+++ Lab12/lab_12.py
@@ -1,5 +1,4 @@
 def external_multiphase_sort(arr: list) -> list:
-    # fibonacci series is used to find the distance of each phase
     fibonacci_series = [0, 1]  
     while fibonacci_series[-1] < len(arr):  
         fibonacci_series.append(fibonacci_series[-1] + fibonacci_series[-2])  
@@ -11,7 +10,6 @@ def external_multiphase_sort(arr: list) -> list:
                 arr[j], arr[j - distance] = arr[j - distance], arr[j]  
                 j -= distance  
   
-    # Sorting each phase independently
     for distance in fibonacci_series[::-1]:  
         sort_phase(arr, distance)  
     return arr
--- Lab9/lab_9.py
+++ Lab9/lab_9.py
@@ -1,22 +1,20 @@
-
 # from heapq import heapify
-# Процедура для преобразования в двоичную кучу поддерева с корневым узлом i, что является индексом в arr[]. n - размер кучи
 def heapify(arr, n, i):
-    largest = i # Initialize largest as root
-    l = 2 * i + 1   # left = 2*i + 1
-    r = 2 * i + 2   # right = 2*i + 2
-    if l < n and arr[i] < arr[l]:
-        largest = l
-    if r < n and arr[largest] < arr[r]:
-        largest = r
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+    if left < n and arr[i] < arr[left]:
+        largest = left
+    if right < n and arr[largest] < arr[right]:
+        largest = right
     if largest != i:
-        arr[i],arr[largest] = arr[largest],arr[i] # свап
+        arr[i], arr[largest] = arr[largest], arr[i] 
         heapify(arr, n, largest)
 
 def heap_sort(arr:list) -> list:
     for i in range(len(arr), -1, -1):
         heapify(arr, len(arr), i)
     for i in range(len(arr)-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i] # свап 
+        arr[i], arr[0] = arr[0], arr[i] 
         heapify(arr, i, 0)
     return arr
