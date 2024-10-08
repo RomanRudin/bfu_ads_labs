4f0c9d0cf4f86e0b2a58dd71b626310ec3bad5c6
Roman <rudin.roman@bk.ru>
2024-10-08 04:26:09 +0200

    Labs from 9 to 11 (including) were done on C++, couple of mistakes in readme were fixed

--- Semester_1/Labs/Lab11/11.cpp
+++ Semester_1/Labs/Lab11/11.cpp
@@ -1,1 +1,31 @@
-// TODO
+#include <iostream>
+// Can't use std::swap 'cause I have to swap pointers on array elements
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(arr, low, high);
+
+        quicksort(arr, low, pivot - 1);
+        quicksort(arr, pivot + 1, high);
+    }
+}
