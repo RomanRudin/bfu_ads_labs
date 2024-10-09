76e9ec3dab9bbb6f2f165fbe0baa3448d65a41b0
Roman <rudin.roman@bk.ru>
2024-10-09 02:04:34 +0200

    Some labs were remade to make navigation easier

---
+++ Semester_1/sorting_run_file.py
@@ -0,0 +1,30 @@
+from Tests.sorting_tests import testing_sorting, testing_external_sorting
+from Labs.Lab4.comb_sort import comb_sort
+from Labs.Lab5.insertion_sort import insertion_sort
+from Labs.Lab6.selection_sort import selection_sort
+from Labs.Lab7.shell_sort import shell_sort
+from Labs.Lab8.radix_sort import radix_sort
+from Labs.Lab9.heap_sort import heap_sort
+from Labs.Lab10.merge_sort import merge_sort
+from Labs.Lab11.quick_sort import quick_sort
+from Labs.Lab12.external_multiphase_sort import external_multiphase_sort
+
+if __name__ == "__main__":
+    print("\nComb sort:")
+    testing_sorting(comb_sort, max_len=10000, testing_amount=20)
+    print("\nInsertion sort:")
+    testing_sorting(insertion_sort, max_len=10000, testing_amount=20)
+    print("\nSelection sort:")
+    testing_sorting(selection_sort, max_len=10000, testing_amount=20)
+    print("\nShell sort:")
+    testing_sorting(shell_sort, max_len=10000, testing_amount=20)
+    print("\nRadix sort:")
+    testing_sorting(radix_sort, max_len=10000, testing_amount=20)
+    print("\nHeap sort:")
+    testing_sorting(heap_sort, max_len=10000, testing_amount=20) 
+    print("\nMerge sort:")
+    testing_sorting(merge_sort, max_len=10000, testing_amount=20)
+    print("\nQuick sort:")
+    testing_sorting(quick_sort, max_len=10000, testing_amount=20)
+    print("\nExternal multiphase sort:")
+    testing_external_sorting(external_multiphase_sort(), "../Labs/Lab12/Data", max_len=10000, testing_amount=20)
