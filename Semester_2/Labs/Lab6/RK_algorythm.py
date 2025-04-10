4ed53b1a76f2b4efe60627ea9c067004ebdc1ebe
Roman <rudin.roman@bk.ru>
2025-04-10 10:56:07 +0200

    Lab6 was done

--- Semester_2/Labs/Lab6/RK_algorythm.py
+++ Semester_2/Labs/Lab6/RK_algorythm.py
@@ -1,21 +1,15 @@
-# def compareText(text: str, index: int, pattern: str) -> bool:
-#     for i, sym in enumerate(pattern):
-#         if (sym != text[index + i]):
-#             return False
-#     return True
-
-
 def rk_algorythm(text: str, pattern: str) -> list[int]:
     result = []
     patternHash = hash(pattern)
 
     for i in range(len(text) - len(pattern) + 1):
         textHash = hash(text[i:(len(pattern) + i)])
-        # print(f"i = {i} \t {pattern} : {patternHash}, \t {text[i:(len(pattern) + i)]} : {textHash}")
         if ((patternHash == textHash) and all([pattern[j] == text[i + j] for j in range(len(pattern))])):
             result.append(i)
     return result
 
-# if __name__ == "__main__":
-#     print(f"Result of rk_algorythm: {rk_algorythm('abca', 'abca')}")
-#     print(f"Result of rk_algorythm: {rk_algorythm('abcababdabaaba', 'abaaba')}")
+if __name__ == "__main__":
+    print(f"Result of rk_algorythm: {rk_algorythm('abca', 'abca')}")
+    print(f"Result of rk_algorythm: {rk_algorythm('abcababdabaaba', 'abaaba')}")
+    print(f"Result of rk_algorythm: {rk_algorythm('abcababdabaab', 'abaaba')}")
+    print(f"Result of rk_algorythm: {rk_algorythm('abcababdabaab', 'ab')}")
