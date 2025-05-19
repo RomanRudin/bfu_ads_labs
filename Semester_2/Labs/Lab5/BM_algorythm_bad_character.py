f539b078339bd0269eaab3991f7d6e3d982d4075
Петр Алай (старина) <92215614+PELMENAP@users.noreply.github.com>
2025-05-20 01:02:37 +0200

    Update BM_algorythm_bad_character.py
    Упрощение условий и корректировка названий

--- Semester_2/Labs/Lab5/BM_algorythm_bad_character.py
+++ Semester_2/Labs/Lab5/BM_algorythm_bad_character.py
@@ -1,42 +1,35 @@
-def get_bad_characters_dict(pattern: str) -> dict[str, int]:
-    bad_characters_dict = {}
+def preprocess_bad_char(pattern: str) -> dict[str, int]:
+    bad_char = {}
+    for i, char in enumerate(pattern):
+        bad_char[char] = i
+    return bad_char
 
-    for i in range(len(pattern)):
-        bad_characters_dict[pattern[i]] = i
-
-    return bad_characters_dict
-
-
-
-def bm_algorythm_bad_character(text: str, pattern: str) -> list[int]:
-    symbol_indexes = get_bad_characters_dict(pattern)
+def bm_search(text: str, pattern: str) -> list[int]:
+    if not pattern or not text:
+        return []
+    
+    bad_char = preprocess_bad_char(pattern)
+    m = len(pattern)
+    n = len(text)
     result = []
+    
     shift = 0
-
-    while (shift <= (len(text) - len(pattern))):
-        current_index = len(pattern) - 1
-        while (current_index >= 0 and pattern[current_index] == text[shift + current_index]):
-            current_index -= 1
-
-        if (current_index == -1):
+    while shift <= n - m:
+        j = m - 1
+        
+        while j >= 0 and pattern[j] == text[shift + j]:
+            j -= 1
+            
+        if j < 0:
             result.append(shift)
-            if (shift + len(pattern)) < len(text):
-                if text[shift + len(pattern)] in symbol_indexes.keys():
-                    shift -= symbol_indexes[text[shift + len(pattern)]]
-                shift += len(pattern)
-            else:
-                shift += 1
-
+            shift += (m - bad_char.get(text[shift + m], -1)) if shift + m < n else 1
         else:
-            if text[shift + current_index] in symbol_indexes.keys():
-                indent = symbol_indexes[text[shift + current_index]]
-            else:
-                indent = -1
-            shift += max(1, current_index - indent)
-
+            char = text[shift + j]
+            shift += max(1, j - bad_char.get(char, -1))
+    
     return result
 
 if __name__ == "__main__":
-    print(f"Result of bm_algorythm: {bm_algorythm_bad_character('abca', 'abca')}")
-    print(f"Result of bm_algorythm: {bm_algorythm_bad_character('abcababdabaaba', 'abaaba')}")
-    print(f"Result of bm_algorythm: {bm_algorythm_bad_character('abcababdabaabadd', 'abaaba')}")
+    print(f"Result of bm_algorythm: {bm_search('abca', 'abca')}")
+    print(f"Result of bm_algorythm: {bm_search('abcababdabaaba', 'abaaba')}")
+    print(f"Result of bm_algorythm: {bm_search('abcababdabaabadd', 'abaaba')}")
