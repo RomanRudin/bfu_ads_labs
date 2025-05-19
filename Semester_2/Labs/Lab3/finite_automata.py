1f3ca35b06c06a76825b5d373a51d030e1ca83a6
PELMENAP <92215614+PELMENAP@users.noreply.github.com>
2025-05-20 00:29:52 +0200

    Improve finite_automata.py
    build_transition_table:
    Ненужное условие, обобщение для любых символов
    
    finite_automata:
    Граничные условия и поиск всех вхождений

--- Semester_2/Labs/Lab3/finite_automata.py
+++ Semester_2/Labs/Lab3/finite_automata.py
@@ -1,28 +1,36 @@
 def build_transition_table(pattern: str) -> list[dict]:
     m = len(pattern)
+    unique_chars = set(pattern)
     transition_table = [{} for _ in range(m + 1)]
     
     for state in range(m + 1):
-        for c in ['a', 'b', 'c']:
+        for c in unique_chars:
             next_state = min(m, state + 1)
-            while next_state > 0 and (next_state - 1 >= len(pattern) or pattern[next_state - 1] != c):
+            while next_state > 0 and pattern[next_state - 1] != c:
                 next_state = transition_table[next_state - 1].get(c, 0)
             transition_table[state][c] = next_state
     
     return transition_table
 
 def finite_automata(text: str, pattern: str) -> None:
+    if len(pattern) == 0:
+        return list(range(len(text) + 1))
+    if len(text) < len(pattern):
+        return []
+    
     transition_table = build_transition_table(pattern)
     n = len(text)
     m = len(pattern)
     state = 0
     
-    for i in range(n):
-        current_char = text[i]
-        state = transition_table[state].get(current_char, 0)
-        
+    occurrences = []
+    
+    for i, c in enumerate(text):
+        state = transition_table[state].get(c, 0)
         if state == m:
-            return i - m + 1
+            occurrences.append(i - m + 1)
+    
+    return occurrences
 
 
 
@@ -30,3 +38,4 @@ if __name__ == "__main__":
     print(f"Result of bm_algorythm: {finite_automata('abca', 'abca')}")
     print(f"Result of bm_algorythm: {finite_automata('abcababdabaaba', 'abaaba')}")
     print(f"Result of bm_algorythm: {finite_automata('abcababdabaabadd', 'abaaba')}")
+    print(f"Result of bm_algorythm: {finite_automata('abcababdabaabadd', 'aba')}")
